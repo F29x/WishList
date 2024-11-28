f35de8ee1b5e65b9de3717e4c928f703038418f9
F29x <f.dhima29@gmail.com>
2024-11-28 18:49:51 +0100

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishList } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  item =[
+    new WishList('to learn angular'),
+    new WishList('have some coffe', true),
+    new WishList('welcome santa claus')
+  ];
   title = 'WishList';
 }
